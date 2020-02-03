5ac9a96267400f6eeeff71720577b4f764f12412
David Dixon <chimichanga@g.ucla.edu>
2020-02-03 10:37:05 -0800

    add dependent destroy to follower relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,11 @@ class User < ApplicationRecord
 
     has_many :posts, class_name: :Post, primary_key: :id, foreign_key: :user_id
 
-    has_many :follows
-    has_many :follower_relationships, foreign_key: :following_id, class_name: :Follow
+    has_many :follows, dependent: :destroy
+    has_many :follower_relationships, foreign_key: :following_id, class_name: :Follow, dependent: :destroy
     has_many :followers, through: :follower_relationships, source: :follower
 
-    has_many :following_relationships, foreign_key: :user_id, class_name: :Follow
+    has_many :following_relationships, foreign_key: :user_id, class_name: :Follow, dependent: :destroy
     has_many :followings, through: :following_relationships, source: :following
     has_many :following_images, through: :followings, source: :posts
 
