8d6dd60583893e180d1775410f5a2df999be261d
David Dixon <chimichanga@g.ucla.edu>
2020-02-04 12:28:25 -0800

    modify searchbar reducer

--- frontend/reducers/search_reducer.js
+++ frontend/reducers/search_reducer.js
@@ -4,8 +4,8 @@ import { RECEIVE_USERS } from "../actions/user_actions"
 const SearchBarReducer = (state = {}, action) => {
     Object.freeze(state);
     switch(action.type){
-        case TESTER:
-            return Object.assign({}, )
+        case RECEIVE_USERS:
+            return Object.assign({}, action.users)
         default:
             return state
     }
