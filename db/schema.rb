6b21267c51e865315f6ac7f37675a4510fe97452
David Dixon <chimichanga@g.ucla.edu>
2020-02-06 15:00:13 -0800

    create likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_002727) do
+ActiveRecord::Schema.define(version: 2020_02_06_224615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 2020_02_06_002727) do
     t.index ["following_id"], name: "index_follows_on_following_id"
     t.index ["user_id", "following_id"], name: "index_follows_on_user_id_and_following_id", unique: true
     t.index ["user_id"], name: "index_follows_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id", "user_id"], name: "index_likes_on_post_id_and_user_id", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
