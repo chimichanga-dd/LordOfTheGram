3fd441c109130e6519e4fcd5f63e093dc50c7afe
David Dixon <chimichanga@g.ucla.edu>
2020-02-05 19:37:44 -0800

    update errors reducer to clear after logging in

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,10 +1,11 @@
-import {RECEIVE_ERRORS, RECEIVE_USER} from "../actions/session_actions"
+import { RECEIVE_ERRORS, RECEIVE_CURRENT_USER} from "../actions/session_actions"
+
 
 const ErrorsReducer = (state = {}, action) => {
     switch(action.type){
         case RECEIVE_ERRORS:
             return Object.assign({}, action.errors)
-        case RECEIVE_USER:
+        case RECEIVE_CURRENT_USER:
             return []
         default:
             return state
