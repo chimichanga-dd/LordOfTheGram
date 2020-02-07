0ccb929a290ad555ba115686469ba1bdb8f09de7
David Dixon <chimichanga@g.ucla.edu>
2020-02-06 16:41:05 -0800

    add ajax functions for likes

---
+++ frontend/util/likes_util.js
@@ -0,0 +1,18 @@
+
+
+export const createLike = (like) => (
+    $.ajax({
+        method: "POST",
+        url: "api/likes",
+        data: {like}
+    })
+)
+
+export const deleteLike = (likeId) => (
+    $.ajax({
+        method: "DELETE",
+        url: `api/likes/${likeId}`
+    })
+)
+
+export const 
