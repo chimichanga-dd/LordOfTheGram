db513277d22379737f44063fcc969a48d9a3a623
David Dixon <chimichanga@g.ucla.edu>
2020-02-08 10:49:33 -0800

    add comment actions

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,23 @@
+
+import * as CommentUtil from "../util/comments_util"
+
+export const RECEIVE_COMMENT = "RECEIVE_COMMENT"
+export const REMOVE_COMMENT = "REMOVE_COMMENT"
+
+const receiveComment = (comment) => ({
+    type: RECEIVE_COMMENT,
+    comment
+})
+
+const removeComment = (comment) => ({
+    type: REMOVE_COMMENT,
+    comment
+})
+
+export const createComment = (comment) => (dispatch) => {
+    CommentUtil.createComment(comment).then( (comment) => dispatch(receiveComment(comment)) )
+}
+
+export const deleteComment = (commentId) => (dispatch) => {
+    CommentUtil.deleteComment(commentId).then( (comment) => dispatch(removeComment(comment)) )
+}
