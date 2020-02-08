d36dc65fcd380861dabb6f4e1c489ec7a2fdb1e5
David Dixon <chimichanga@g.ucla.edu>
2020-02-08 11:11:18 -0800

    add comments to post reducer

--- frontend/reducers/post_reducer.js
+++ frontend/reducers/post_reducer.js
@@ -1,6 +1,7 @@
 import { RECEIVE_POST, RECEIVE_POSTS, REMOVE_POST } from "../actions/post_actions"
 import { LOGOUT_CURRENT_USER } from "../actions/session_actions"
 import { RECEIVE_LIKE, REMOVE_LIKE } from "../actions/like_actions"
+import { RECEIVE_COMMENT, REMOVE_COMMENT } from "../actions/comment_actions"
 
 const postReducer = (state = {}, action) => {
     Object.freeze(state)
@@ -23,6 +24,14 @@ const postReducer = (state = {}, action) => {
             post = new_state[action.like.post_id]
             post.likers = post.likers.filter((id) => id != action.like.user_id)
             return Object.assign(new_state)
+        case RECEIVE_COMMENT:
+            post = new_state[action.comment.post_id]
+            post.comments[action.comment.id] = action.comment
+            return new_state
+        case REMOVE_COMMENT:
+            post = new_state[action.comment.post_id]
+            delete post.comments[action.comment.id]
+            return new_state
         case LOGOUT_CURRENT_USER:
             return {}
         default:
