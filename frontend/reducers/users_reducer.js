1b96ce8f9246e84a70d538feacf94e8b4569e5d9
David Dixon <chimichanga@g.ucla.edu>
2020-02-08 11:20:15 -0800

    add comments to user reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -3,14 +3,15 @@ import { RECEIVE_USER } from "../actions/user_actions"
 import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions"
 import { RECEIVE_LIKE, REMOVE_LIKE } from "../actions/like_actions"
 import { RECEIVE_FOLLOW, REMOVE_FOLLOW } from "../actions/follow_actions"
+import { RECEIVE_COMMENT, REMOVE_COMMENT } from "../actions/comment_actions"
 
 const UsersReducer = (state =  {}, action) => {
     Object.freeze(state)
-    let nextState = Object.assign({}, state)
-    let current_user, follow_target,like_receiver
+    let next_state = Object.assign({}, state)
+    let current_user, follow_target,receiver
     switch(action.type){
         case RECEIVE_USER:
-            return Object.assign(nextState, {[action.user.id]: action.user})
+            return Object.assign(next_state, {[action.user.id]: action.user})
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, {[action.user.id]: action.user})
         case LOGOUT_CURRENT_USER:
@@ -18,27 +19,39 @@ const UsersReducer = (state =  {}, action) => {
         case RECEIVE_FOLLOW:
             current_user = action.follow.user_id
             follow_target = action.follow.following_id
-            nextState[current_user].following.push(action.follow.following_id)
-            nextState[follow_target].followers.push(action.follow.user_id)
-            return nextState
+            next_state[current_user].following.push(action.follow.following_id)
+            next_state[follow_target].followers.push(action.follow.user_id)
+            return next_state
         case REMOVE_FOLLOW:
-            current_user = nextState[action.follow.user_id]
-            follow_target = nextState[action.follow.following_id]
+            current_user = next_state[action.follow.user_id]
+            follow_target = next_state[action.follow.following_id]
             current_user.following = current_user.following.filter(id => id !== action.follow.following_id)
             follow_target.followers = follow_target.followers.filter(id => id !== action.follow.user_id)
-            return nextState
+            return next_state
         case RECEIVE_LIKE:
-            like_receiver = nextState[action.like.receiver]
-            if (like_receiver) {
-                like_receiver.posts[action.like.post_id].likers.push(action.like.user_id)
+            receiver = next_state[action.like.receiver]
+            if (receiver) {
+                receiver.posts[action.like.post_id].likers.push(action.like.user_id)
             }
-            return nextState
+            return next_state
         case REMOVE_LIKE:
-            like_receiver = nextState[action.like.receiver]
-            if (like_receiver) {
-                like_receiver.posts[action.like.post_id].likers = like_receiver.posts[action.like.post_id].likers.filter((id) => id != action.like.user_id)
+            receiver = next_state[action.like.receiver]
+            if (receiver) {
+                receiver.posts[action.like.post_id].likers = receiver.posts[action.like.post_id].likers.filter((id) => id != action.like.user_id)
             }
-            return nextState
+            return next_state
+        case RECEIVE_COMMENT:
+            receiver = next_state[action.comment.receiver]
+            if (receiver){
+                receiver.posts[action.comment.post_id].comments[action.comment.id] = action.comment
+            }
+            return next_state
+        case REMOVE_COMMENT:
+            receiver = next_state[action.comment.receiver]
+            if (receiver) {
+                delete receiver.posts[action.comment.post_id].comments[action.comment.id]
+            }
+            return next_state
         default:
             return state
     }
