8d6b7031d7b0af705b3ef49e4b51ffdb40d19683
David Dixon <chimichanga@g.ucla.edu>
2020-02-09 10:25:55 -0800

    add state and dispatch to component props

--- frontend/components/post/comment/comment_container.js
+++ frontend/components/post/comment/comment_container.js
@@ -1,13 +1,16 @@
 import { connect } from "react-redux"
 import CommentItem from "./comment_item"
+import { createComment, deleteComment } from "../../../actions/comment_actions"
 
 
-const mapsStateToProps = (state) => ({
-
+const mapsStateToProps = (state, ownProps) => ({
+    currentUser: state.session.id,
+    comments: state.entities.posts[ownProps.postId].comments
 })
 
 const mapDispatchToProps = (dispatch) => ({
-
+    createComment: (comment) => dispatch(createComment(comment)),
+    deleteComment: (commentId) => dispatch(deleteComment(commentId))
 })
 
 export default connect(mapsStateToProps,mapDispatchToProps)(CommentItem)
