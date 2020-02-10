2ffc0b7037412fd35169fffb863011a19d95ca18
David Dixon <chimichanga@g.ucla.edu>
2020-02-09 17:29:35 -0800

    add phone image to session container

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -27,25 +27,28 @@ class SessionForm extends React.Component{
 
     render(){
         
-        return(            
-            <form className="session-form" onSubmit={this.handleSubmit}>
-            {this.props.errors.map((error,idx) => (<div key={`error-${idx+1}`} className="error">{error}</div>))}
-                <label htmlFor="session-form-username">Username: </label>
-                <input 
-                    type="text"
-                    id="session-form-username"
-                    value={this.state.username}
-                    onChange={this.handleInput("username")}
-                /><br/>
-                <label htmlFor="session-form-password">Password: </label>
-                <input
-                    type="password"
-                    id="session-form-password"
-                    value={this.state.password}
-                    onChange={this.handleInput("password")}
-                />
-                <button>{this.props.formAction}</button>
-            </form>
+        return(
+            <div>
+                <img src={window.images.gram_phone} alt="gram screenshot displayed on phone"/>
+                <form className="session-form" onSubmit={this.handleSubmit}>
+                {this.props.errors.map((error,idx) => (<div key={`error-${idx+1}`} className="error">{error}</div>))}
+                    <label htmlFor="session-form-username">Username: </label>
+                    <input 
+                        type="text"
+                        id="session-form-username"
+                        value={this.state.username}
+                        onChange={this.handleInput("username")}
+                    /><br/>
+                    <label htmlFor="session-form-password">Password: </label>
+                    <input
+                        type="password"
+                        id="session-form-password"
+                        value={this.state.password}
+                        onChange={this.handleInput("password")}
+                    />
+                    <button>{this.props.formAction}</button>
+                </form>
+            </div>
         )
     }
 
