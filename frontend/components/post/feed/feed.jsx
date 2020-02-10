b7c577e126c80c15c7c9b1ffd688c2a70ee8387e
David Dixon <chimichanga@g.ucla.edu>
2020-02-10 11:57:39 -0800

    create throttled scroll event to fetch posts

--- frontend/components/post/feed/feed.jsx
+++ frontend/components/post/feed/feed.jsx
@@ -1,6 +1,7 @@
 
 import React from "react"
 import IndexItemContainer from "./feed_item_container"
+import throttle from "lodash/throttle"
 
 class Feed extends React.Component{
 
@@ -11,9 +12,25 @@ class Feed extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            offset: 2
+            offset: 0
         }
-    }
+
+        let fetchMorePosts = () => {
+            let {innerHeight} = window
+            let {scrollTop, scrollHeight} = document.documentElement
+            let distanceToBottom = scrollHeight - scrollTop - innerHeight
+
+            if (distanceToBottom < 100){
+                this.setState({offset: this.state.offset + 5})
+                this.props.fetchPosts(this.state.offset)
+            }
+        }
+        let throttled = throttle(fetchMorePosts, 2000)
+        window.addEventListener("scroll", throttled)
+    };
+
+
+
 
     render(){
         let {posts} = this.props
