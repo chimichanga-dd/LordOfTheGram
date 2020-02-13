c94d20436c2157948f218487a06520a374bd829e
David Dixon <chimichanga@g.ucla.edu>
2020-02-12 22:01:53 -0800

    change remove button to image

--- frontend/components/post/comment/comment_item.jsx
+++ frontend/components/post/comment/comment_item.jsx
@@ -22,7 +22,10 @@ class CommentItem extends React.Component {
 
     renderDeleteButton(commenterId, commentId) {
         if (this.props.currentUser == this.props.postOwnerId || this.props.currentUser == commenterId) {
-            return <button onClick={ () => this.props.deleteComment(commentId)}> DELETE</button>
+            return <img className="button-remove"
+                        src={window.images.remove}
+                        onClick={ () => this.props.deleteComment(commentId)}
+                    />
         } else {
             return null
         }
@@ -35,14 +38,9 @@ class CommentItem extends React.Component {
             return Object.values(comments).map((comment, idx) => {
                 return (
                     <ul className="comment-container" key={`container-${idx}`}>
-                        <li className="comment-photo" key={`photo-${idx}`}>
-                            <Link to={`users/${comment.user_id}`}>
-                                <img src={comment.user_photo} height="50px" width="50px" />
-                            </Link>
-                        </li>
                         <li className="comment-username" key={`username-${idx}`}>
                             <Link to={`users/${comment.user_id}`}>
-                                <p>{comment.user}</p>
+                                <p className="bold">{comment.user}</p>
                             </Link>
                         </li>
                         <li className="comment-text" key={`text-${idx}`}>{comment.text}</li>
