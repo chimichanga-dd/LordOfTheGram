48e01d4dc2bf8e34d6edfc40c5c819cc1c607ef2
David Dixon <chimichanga@g.ucla.edu>
2020-02-14 15:17:10 -0800

    delete users from follow reducer when followed

--- frontend/reducers/follow_reducer.js
+++ frontend/reducers/follow_reducer.js
@@ -3,10 +3,20 @@ import {RECEIVE_FOLLOW, REMOVE_FOLLOW, RECEIVE_NOT_FOLLOWED} from "../actions/fo
 
 const followReducer = (state = {}, action) => {
     Object.freeze(state)
-
+    let new_state = Object.assign({}, state)
     switch(action.type){
         case RECEIVE_NOT_FOLLOWED:
             return action.users
+        case RECEIVE_FOLLOW:
+            let followedId = action.follow.following_id
+            let notFollowing = Object.values(new_state)
+            for (let i = 0; i < notFollowing.length; i++){
+                if (notFollowing[i].id == followedId){
+                    delete new_state[notFollowing[i].username]
+                    return new_state
+                }
+            }
+            return new_state
         default:
             return state
     }
