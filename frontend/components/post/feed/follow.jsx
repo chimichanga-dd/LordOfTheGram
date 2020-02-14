fe74088d8c324ad2adac57caf0bfa734bcad3a11
David Dixon <chimichanga@g.ucla.edu>
2020-02-14 11:06:03 -0800

    create follow_container and follow item

---
+++ frontend/components/post/feed/follow.jsx
@@ -0,0 +1,17 @@
+
+import React from "react"
+
+class FollowItem extends React.Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>NOT FOLLOWING</div>
+        )
+    }
+}
+
+export default FollowItem
---
+++ frontend/components/post/feed/follow_container.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux"
+import FollowItem from "./follow"
+
+
+const mapStateToProps = () => {
+
+}
+
+
+const mapDispatchToProps = () => {
+
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(FollowItem)
