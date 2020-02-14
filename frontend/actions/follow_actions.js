0cc52bd6a33c3c344bfb3b40d4b9adbc99862e4f
David Dixon <chimichanga@g.ucla.edu>
2020-02-14 11:28:43 -0800

    add actions for getting non-followed users

--- frontend/actions/follow_actions.js
+++ frontend/actions/follow_actions.js
@@ -3,7 +3,7 @@ import * as FollowUtil from "../util/follow_util"
 
 export const RECEIVE_FOLLOW = "RECEIVE_FOLLOW";
 export const REMOVE_FOLLOW = "REMOVE_FOLLOW";
-
+export const RECEIVE_NOT_FOLLOWED = "RECEIVE_NOT_FOLLOWED"
 
 const receiveFollow = (follow) => ({
     type: RECEIVE_FOLLOW,
@@ -15,6 +15,11 @@ const removeFollow = (follow) => ({
     follow
 })
 
+const receiveNotFollowed = (users) => ({
+    type: RECEIVE_NOT_FOLLOWED,
+    users
+})
+
 export const createFollow = (follow) => (dispatch) => (
     FollowUtil.createFollow(follow).then( 
         (follow) => dispatch(receiveFollow(follow))
@@ -24,5 +29,11 @@ export const createFollow = (follow) => (dispatch) => (
 export const deleteFollow = (followingId) => (dispatch) => (
     FollowUtil.deleteFollow(followingId).then( 
         (follow) => dispatch(removeFollow(follow))
+    )
+)
+
+export const getNotFollowed = () => (dispatch) => (
+    FollowUtil.getPeople().then(
+        (users) => dispatch(receiveNotFollowed(users))
     )
 )
