7ca4ffb45fdc3a99a8c1a6645bd19188f3ed3598
David Dixon <chimichanga@g.ucla.edu>
2020-02-18 11:07:04 -0800

    add modal reducer to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,12 +4,14 @@ import usersReducer from "./users_reducer"
 import postReducer from "./post_reducer";
 import searchReducer from "./search_reducer"
 import followReducer from "./follow_reducer";
+import modalReducer from "./modal_reducer"
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     posts: postReducer,
     search: searchReducer,
-    follow: followReducer
+    follow: followReducer,
+    modal: modalReducer
 })
 
 export default entitiesReducer
