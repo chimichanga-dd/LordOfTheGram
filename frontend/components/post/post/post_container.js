1e8b79e5727e8c51b8d9ce53fd238d1a47bcc69d
David Dixon <chimichanga@g.ucla.edu>
2020-02-18 16:23:24 -0800

    create props for post

--- frontend/components/post/post/post_container.js
+++ frontend/components/post/post/post_container.js
@@ -4,10 +4,18 @@ import Post from "./post"
 
 
 const mapStateToProps = (state) => {
-
+    const currentUserId = state.session.id
+    const posterId = state.entities.modal.posterId
+    const postId = state.entities.modal.postId
+    const poster = state.entities.users[posterId]
+    const post = poster.posts[postId]
 
     return ({
-
+        currentUserId,
+        posterId,
+        postId,
+        poster,
+        post,
     })
 }
 
