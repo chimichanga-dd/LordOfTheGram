91824f65b2cbd6dd591758d5eb33adeca1dd15d3
David Dixon <chimichanga@g.ucla.edu>
2020-02-18 16:15:23 -0800

    create post and container skeleton

---
+++ frontend/components/post/post/post.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+class Post extends React.Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        <div className="photo-container">
+            <p>here is a photo</p>
+        </div>
+    }
+}
+
+export default Post
---
+++ frontend/components/post/post/post_container.js
@@ -0,0 +1,18 @@
+import { connect } from "react-redux"
+import Post from "./post"
+
+
+
+const mapStateToProps = (state) => {
+
+
+    return ({
+
+    })
+}
+
+const mapDispatchToProps = (dispatch) => ({
+
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(Post)
