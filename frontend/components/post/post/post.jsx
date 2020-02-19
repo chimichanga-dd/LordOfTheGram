1912e98068e50490650df7b51eb28728a3f57614
David Dixon <chimichanga@g.ucla.edu>
2020-02-18 17:47:25 -0800

    add author pic and link to post modal

--- frontend/components/post/post/post.jsx
+++ frontend/components/post/post/post.jsx
@@ -13,20 +13,25 @@ class Post extends React.Component{
         return (
             <div className="post-container">
                 <img className="photo" src={post.photo_url} alt=""/>
-                <div classname="photo-right">
+                <div className="photo-right">
                     <div className="poster-link-container">
                         <a
-                            className="poster-link"
+                            className="poster-link-username"
                             href={`#/users/${poster.id}`}
                             key={poster.id}
                         >
-                            <label className="poster-link-person">
-                                <img
-                                    src={poster.picture}
-                                    alt={`profile picture of ${poster}`}
-                                />
-                                <p className="bold">{poster.username}</p>
-                            </label>
+                            <img
+                                className="poster-link-image"
+                                src={poster.picture}
+                                alt={`profile picture of ${poster}`}
+                            />
+                        </a>
+                        <a
+                            className="poster-link-username"
+                            href={`#/users/${poster.id}`}
+                            key={poster.id}
+                        >
+                            <p className="bold">{poster.username}</p>
                         </a>
                     </div>
 
