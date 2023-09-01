5e6373c595b6a3814f4bbf6facd1b687a824752f
David Dixon <chimichanga@g.ucla.edu>
2023-09-01 22:48:39 +0700

    use Link to fix navigation issue

--- frontend/components/post/feed/follow.jsx
+++ frontend/components/post/feed/follow.jsx
@@ -1,70 +1,79 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
-import React from "react"
+class FollowItem extends React.Component {
+  constructor(props) {
+    super(props);
 
-class FollowItem extends React.Component{
+    this.renderFollowButton = this.renderFollowButton.bind(this);
+    this.renderNotFollowed = this.renderNotFollowed.bind(this);
+  }
 
-    constructor(props){
-        super(props)
+  componentDidMount() {
+    this.props.getNotFollowed();
+  }
 
-        this.renderFollowButton = this.renderFollowButton.bind(this)
-        this.renderNotFollowed = this.renderNotFollowed.bind(this)
+  renderFollowButton(user) {
+    if (!this.props.following.includes(user.id)) {
+      return (
+        <button
+          className="bold"
+          onClick={() =>
+            this.props.createFollow({
+              user_id: this.props.currentUserId,
+              following_id: user.id
+            })
+          }
+        >
+          Follow
+        </button>
+      );
+    } else {
+      return null;
     }
+  }
 
-    componentDidMount(){
-        this.props.getNotFollowed()
+  renderNotFollowed() {
+    let output;
+    if (this.props.notFollowed.length > 0) {
+      output = this.props.notFollowed.map((user) => {
+        return (
+          <li
+            className="not-followed-user-container"
+            key={user.id}
+          >
+            <Link
+              className="search-link"
+              to={`/users/${user.id}`}
+            >
+              <label className="search-link-person">
+                <img
+                  src={user.picture}
+                  alt={`profile picture of ${user}`}
+                />
+                <p className="bold">{user.username}</p>
+              </label>
+            </Link>
+            {this.renderFollowButton(user)}
+          </li>
+        );
+      });
+    } else {
+      output = (
+        <p className="not-followed-user-container">You follow every user</p>
+      );
     }
-    
-    renderFollowButton(user){
-        if (!this.props.following.includes(user.id)){
-            return (
-                <button className="bold" onClick={() => 
-                    this.props.createFollow({ user_id: this.props.currentUserId, following_id: user.id })
-                }>Follow</button>
-            )
-        } else {
-            return null
-        }
-    }
+    return output;
+  }
 
-    renderNotFollowed(){
-        let output;
-        if (this.props.notFollowed.length > 0){
-            output = this.props.notFollowed.map(
-                    (user) => {
-                        return (
-                            <li className="not-followed-user-container" key={user.id}>
-                                <a
-                                    className="search-link"
-                                    href={`#/users/${user.id}`}
-                                >
-                                    <label className="search-link-person">
-                                        <img
-                                            src={user.picture}
-                                            alt={`profile picture of ${user}`}
-                                        />
-                                        <p className="bold">{user.username}</p>
-                                    </label>
-                                </a>
-                                {this.renderFollowButton(user)}
-                            </li>
-                        )
-                    }
-            )
-        } else {
-            output = <p className="not-followed-user-container">You follow every user</p>
-        }
-        return output;
-    }
-
-
-    render(){
-        return(
-            <ul className="not-followed-container">
-                <h2 className="bold">Suggestions For You</h2>
-                {this.renderNotFollowed()}
-            </ul>
-        )
-    }
+  render() {
+    return (
+      <ul className="not-followed-container">
+        <h2 className="bold">Suggestions For You</h2>
+        {this.renderNotFollowed()}
+      </ul>
+    );
+  }
 }
 
-export default FollowItem
+export default FollowItem;
